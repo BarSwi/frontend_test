7f18382b52a7527ac9f880ffed42dba440fae9a2
user <bartosz.switala.01@gmail.com>
2024-07-10 11:01:58 +0200

    GetItem method changed for better maintability and readability

--- src/components/Content/ThirdBlock.vue
+++ src/components/Content/ThirdBlock.vue
@@ -3,31 +3,43 @@ import EventBus from '@/EventBus';
 import { ref, computed } from 'vue';
 const sentences = ref([]);
 
-const getItem = (option) =>{
-    let newItem = '';
-    const sentencesStorage = JSON.parse(localStorage.getItem('sentences'));
-    if(option==='option1'){
-        newItem = sentencesStorage[0];
-    }
-    else if(option==='option2'){
-        newItem = sentencesStorage[1];
-    }
-    if(option==='option3'){
-        let firstFlag = true;
-        const secondFlag = sentences.value.length < sentencesStorage.length;
+    const getItemByIndex = (index, storage) => {
+        return storage[index];
+    };
 
-        if(!secondFlag){
-            alert("Nie ma z czego losować");
+    const getRandomItem = (storage) => {
+        if (sentences.value.length >= storage.length) {
+            alert("Nie ma z czego losować.");
             return;
         }
-        while(firstFlag){
-            const randomIndex = Math.floor(Math.random() * sentencesStorage.length);
-            newItem = sentencesStorage[randomIndex];
-            firstFlag = sentences.value.includes(newItem) ? true : false;
+
+        let newItem = '';
+        let foundFlag = false;
+
+        while (!foundFlag) {
+            const randomIndex = Math.floor(Math.random() * storage.length);
+            newItem = storage[randomIndex];
+            foundFlag = !sentences.value.includes(newItem);
         }
+
+        return newItem;
+    };
+
+    const getItem = (option) =>{
+        const sentencesStorage = JSON.parse(localStorage.getItem('sentences'));
+        let newItem = '';
+
+        if (option === 'option1') {
+            newItem = getItemByIndex(0, sentencesStorage);
+        } else if (option === 'option2') {
+            newItem = getItemByIndex(1, sentencesStorage);
+        } else if (option === 'option3') {
+            newItem = getRandomItem(sentencesStorage);
+        }
+
+        return newItem;
     }
-    return newItem;
-}
+    
     //Once again I am using EventBus because the task is small so additional store would be an overkill
     EventBus.$on('addSentence', (option) => {
         let newItem = getItem(option);
